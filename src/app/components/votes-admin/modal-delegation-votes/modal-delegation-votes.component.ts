604260cdb45910c13d4ef505b6646173fd7e413d
Yparejasuarez98 <ypareja@ditech.es>
2024-05-31 15:36:12 -0500

    agregar url api, se agregan funcionalidades al admin, se agregan modelos e imagen nueva

--- src/app/components/votes-admin/modal-delegation-votes/modal-delegation-votes.component.ts
+++ src/app/components/votes-admin/modal-delegation-votes/modal-delegation-votes.component.ts
@@ -4,24 +4,45 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { Enterprise } from '../models/models';
+import { VotesAdminService } from '../services/votes-admin.service';
+import Swal from 'sweetalert2';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-delegation-votes',
   standalone: true,
-  imports: [MatSelectModule, MatIconModule, MatButtonModule, MatDialogModule],
+  imports: [MatSelectModule, MatIconModule, MatButtonModule, MatDialogModule, ReactiveFormsModule],
   templateUrl: './modal-delegation-votes.component.html',
   styleUrl: './modal-delegation-votes.component.css'
 })
 export class ModalDelegationVotesComponent {
 
+  mixta = '';
+  votes = 0;
+  listEnterprise: Enterprise[] = [];
+  company = new FormControl('');
+
   constructor(public dialogRef: MatDialogRef<ModalDelegationVotesComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any, private router: Router) {
+    @Inject(MAT_DIALOG_DATA) public data: Enterprise, private votesAdminService: VotesAdminService) {
 
+    this.votes = this.data.total_votes;
+    this.mixta = this.data.section;
   }
-  mixta = 0;
-  votes = '';
 
-  redirect() {
-    this.router.navigateByUrl('/votos/detalle');
+  getEnterprise() {
+    this.votesAdminService.getEnterprise(this.data.section, 'default', 1, 1000).subscribe({
+      next: (res) => {
+        this.listEnterprise = res;
+      }, error: (err) => {
+        Swal.fire("Error!", err.message, 'error');
+      }
+    });
+  }
+
+  confirm() {
+    this.data.nifSelected = this.company?.value;
+    debugger
+    this.dialogRef.close(this.data);
   }
 }
--- src/app/components/votes-admin/services/votes-admin.service.ts
+++ src/app/components/votes-admin/services/votes-admin.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { ResponseData } from '../../../shared/models/response';
-import { Enterprise } from '../models/models';
+import { Enterprise, EnterpriseAsist, EnterpriseDelegate, Section } from '../models/models';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +12,26 @@ export class VotesAdminService {
   constructor(private http: HttpClient) { }
 
   getEnterprise(section: string, typeVote: string, page: number, totalPage: number): Observable<Enterprise[]> {
-    return this.http.get<ResponseData>(`/api/v1/enterprise?section=${section}&type_vote=${typeVote}&page=${page}&total _page=${totalPage}`).pipe(
+    return this.http.get<ResponseData>(`http://108.175.10.181:5000/api/v1/enterprise?section=${section}&type_vote=${typeVote}&page=${page}&total _page=${totalPage}`).pipe(
       map((res) => {
         return res.data;
       })
     );
+  }
+
+  getSections(): Observable<Section[]> {
+    return this.http.get<ResponseData>(`http://108.175.10.181:5000/api/v1/sections`).pipe(
+      map((res) => {
+        return res.data;
+      })
+    );;
+  }
+
+  updateEnterpriseAsist(data: EnterpriseAsist) {
+    return this.http.patch(`http://108.175.10.181:5000/api/v1/enterprise`, data)
+  }
+
+  updateEnterpriseDelegate(data: EnterpriseDelegate) {
+    return this.http.patch(`http://108.175.10.181:5000/api/v1/enterprise`, data)
   }
 }
